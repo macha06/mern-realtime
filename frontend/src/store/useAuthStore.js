16817d79872a0a828a3b45ff342c85cdeaa5a184
macha06 <fathurrahmansidiq097@gmail.com>
2024-12-15 19:45:58 +0700

    style: update profile and connect it

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -44,7 +44,7 @@ export const useAuthStore = create((set) => ({
       set({ authUser: res.data });
       toast.success("Logged in successfully");
     } catch (error) {
-      toast.error(error.response.data.error);
+      toast.error(error.response.data.message);
     } finally {
       set({ isLoggingIn: false });
     }
@@ -58,5 +58,19 @@ export const useAuthStore = create((set) => ({
     } catch (error) {
       toast.error(error.response.data.message);
     }
+  },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });      
+    }
   }
+  
 }));
