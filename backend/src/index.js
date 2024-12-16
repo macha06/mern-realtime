70e0a21368ebd5de98c043194ad1dfddffe22d2d
macha06 <fathurrahmansidiq097@gmail.com>
2024-12-17 00:19:06 +0700

    feat : realtime with socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,13 @@ import messageRoutes from './routes/message.routes.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,9 +21,9 @@ app.use(
 );
 
 app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes);
+app.use('/api/messages', messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
   connectDB();
 });
